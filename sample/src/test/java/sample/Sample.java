420ab30d714852c234605c2c05b384a5fb6ba43f
Mutturaj Hulagabal <hulagabal@gmail.com>
2019-04-02 23:18:35 +0530

    first

---
+++ sample/src/test/java/sample/Sample.java
@@ -0,0 +1,11 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+	@Test
+	public void sample(){
+		System.out.println("***************Sample***************");
+	}
+
+}
