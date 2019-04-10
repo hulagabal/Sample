102f949a0e286ae4be8a1ffb1261bbb396fa7a5e
Mutturaj Hulagabal <hulagabal@gmail.com>
2019-04-11 00:07:09 +0530

    add more details like verbose,paralles

--- sample/src/test/java/sample/TestngExample.java
+++ sample/src/test/java/sample/TestngExample.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.testng.TestNG;
 import org.testng.xml.XmlClass;
+import org.testng.xml.XmlInclude;
 import org.testng.xml.XmlSuite;
 import org.testng.xml.XmlTest;
 
@@ -13,21 +14,28 @@ public class TestngExample {
 	public static void main(String[] args) {
 		XmlSuite suite = new XmlSuite();
 		suite.setName("MySuite");
-		suite.setParallel("false");
-		suite.setVerbose(1);
+		suite.setParallel("true");
+		suite.setVerbose(3);
 		suite.addListener("sample.MyListener"); // add listener class
 
-		XmlTest test1 = new XmlTest(suite);
+		XmlTest test1 = new XmlTest(suite);//Pass suite
 		test1.setName("Test1");
 		test1.setPreserveOrder("true");
+		test1.setParallel("true");
+		test1.setThreadCount(5);
 
 		XmlTest test2 = new XmlTest(suite);
 		test2.setName("Test2");
 		test2.setPreserveOrder("true");
 
 		XmlClass class1 = new XmlClass(Sample.class);
+		
+		List<XmlInclude> methods=new ArrayList<XmlInclude>();
+		
+		
+		class1.setIncludedMethods(methods);
 		XmlClass class2 = new XmlClass(Sample2.class);
-		// Arraylist to add classes
+		// Array list to add classes
 		List<XmlClass> classes = new ArrayList<XmlClass>();
 		classes.add(class1);
 		classes.add(class2);
@@ -36,7 +44,7 @@ public class TestngExample {
 		test2.setClasses(classes);
 
 		TestNG testng = new TestNG();
-		// arraylist to add uites
+		// array list to add suites
 		List<XmlSuite> suites = new ArrayList<XmlSuite>();
 		suites.add(suite);
 
